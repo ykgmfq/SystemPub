0598d7f6a5bbe9d7885bffafc8911fb7e8d97762
Dennis M. Pöpperl <free-software@dm-poepperl.de>
2025-09-18 10:37:12 +0200

    Fix workflow more (#18)
    * Scripts expect to see the project root dir
    
    * Publish branch on remote
    
    * PR against main

--- .github/scripts/commit-devcontainer-update.sh
+++ .github/scripts/commit-devcontainer-update.sh
@@ -2,6 +2,7 @@
 set -e
 git config --global user.name "github-actions"
 git config --global user.email "github-actions@github.com"
+git config --global push.autoSetupRemote true
 git checkout -b devcontainer-$1
 git add .devcontainer/devcontainer.json
 git commit -m "Update devcontainer to $1"
