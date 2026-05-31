d0ac7f8aa5e688f815ac8fb2e64c685bfe7d64ce
Dennis M. Pöpperl <free-software@dm-poepperl.de>
2026-05-31 09:38:09 +0000

    Sysext push fix

--- .github/scripts/push-sysext.sh
+++ .github/scripts/push-sysext.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -eu
 
+ver=${TAG#v}
+
 for arch in amd64 arm64; do
   config=/tmp/config-${arch}.json
   echo "{\"architecture\":\"${arch}\",\"os\":\"linux\"}" > "$config"
 
   config_arg="${config}:application/vnd.oci.image.config.v1+json"
-  raw=/tmp/systempub-${arch}-${TAG}.raw
+  raw=/tmp/systempub-${arch}-${ver}.raw
   ref="${IMAGE_BASE}:${TAG}-${arch}"
   oras push --config "$config_arg" "$ref" "$raw"
   echo "${arch}_digest=$(oras resolve "$ref")" >> "$GITHUB_OUTPUT"
