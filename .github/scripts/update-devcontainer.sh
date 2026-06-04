85783e46915e0cbadf4b33f03bb6a3399001badc
Dennis M. Pöpperl <free-software@dm-poepperl.de>
2026-06-04 09:02:16 +0000

    Fix devcontainer update

--- .github/scripts/update-devcontainer.sh
+++ .github/scripts/update-devcontainer.sh
@@ -2,8 +2,8 @@
 set -e
 
 # https://regex101.com/r/Gi9d3E/1
-LATEST_TAG=$(curl --silent https://mcr.microsoft.com/v2/devcontainers/go/tags/list | jq --raw-output '.tags[]' | grep --perl-regexp '^1\.\d+$' | sort --version-sort | tail --lines 1)
-CURRENT_TAG=$(jq --raw-output '.image' .devcontainer/devcontainer.json | cut --delimiter=":" --fields=2)
+LATEST_TAG=$(curl --silent https://mcr.microsoft.com/v2/devcontainers/go/tags/list | jq --raw-output '.tags[] | select(test("^1\\.\\d+$"))' | sort --version-sort | tail --lines 1)
+CURRENT_TAG=$(jq --raw-output '.build.args.GO_TAG' .devcontainer/devcontainer.json)
 
 if [ "$CURRENT_TAG" = "$LATEST_TAG" ]; then
   if [ -n "$GITHUB_OUTPUT" ]; then
@@ -11,10 +11,10 @@ if [ "$CURRENT_TAG" = "$LATEST_TAG" ]; then
   fi
   exit 0
 fi
-jq --arg latest_image "mcr.microsoft.com/devcontainers/go:$LATEST_TAG" '.image = $latest_image' .devcontainer/devcontainer.json > /tmp/devcontainer.json
+jq --arg tag "$LATEST_TAG" '.build.args.GO_TAG = $tag' .devcontainer/devcontainer.json > /tmp/devcontainer.json
 mv /tmp/devcontainer.json .devcontainer/
 if [ -n "$GITHUB_OUTPUT" ]; then
-  echo "tag=$LATEST_TAG" >> $GITHUB_OUTPUT
-  echo result=updated >> $GITHUB_OUTPUT
+  echo "tag=$LATEST_TAG" >> "$GITHUB_OUTPUT"
+  echo "result=updated" >> "$GITHUB_OUTPUT"
 fi
-echo $LATEST_TAG
+echo "$LATEST_TAG"
